9e36985f1adb6d3938ed2c59e1659b8e035ffff0
Aaron Chen <1162129136@qq.com>
2017-11-18 22:37:43 +0800

    Eliminate spikes in the wave

--- src/encode/encoder.v
+++ src/encode/encoder.v
@@ -1,17 +1,22 @@
+`timescale 1ns / 1ps
 module encoder
 	(
 		clk,
+		clk_2,
 		reset,
 		valid,
 		data_in,
 		code_out
 	);
 	input clk;
+	input clk_2;
 	input reset;
 	input wire data_in;
 	output reg valid;
 	output code_out;
+	reg code;
 	reg [1:0] data_out;
+	reg count;
 
 	reg [1:0]state;
 
@@ -20,7 +25,7 @@ module encoder
 		valid=0;
 	end
 
-	always@(posedge clk)
+	always@(posedge clk_2 or negedge reset)
 	begin
 		if(!reset)
 			begin
@@ -38,5 +43,24 @@ module encoder
 			end
 	end
 
-	assign code_out= (clk)? data_out[0]: data_out[1];
+	always@(posedge clk or negedge reset)
+	begin
+		if(!reset)
+		begin
+			count <= 0;
+		end
+		else if(count == 0)
+		begin
+			code <= data_out[0];
+			count <= count+1;
+		end
+		else
+		begin
+			code <= data_out[1];
+			count <= count+1;
+		end
+	end
+
+	assign code_out = code;
+	//assign code_out= (clk)? data_out[0]: data_out[1];
 endmodule // encoder
